2bce3ab92f78833e915342ae729d1fed0ca18e7f
Kalana <kalana33471@gmail.com>
2025-10-30 13:35:57 +0530

    setting up the theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-red-600">LiveCoach.AI</h1>
+        <h1 className="bg-primary">LiveCoach.AI</h1>
        </div>
     </>
   )
