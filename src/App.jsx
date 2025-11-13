80f0b0e8eb5c5ed3f9e7763e48859ef490619072
Kalana <kalana33471@gmail.com>
2025-11-13 21:55:35 +0530

    added features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Benefits from "./components/Benefits"
 import Hero from "./components/Hero"
+import Features from "./components/Features"
 
 function App() {
   return (
     <>
       <Hero></Hero>
-      <Benefits/>
+      <Benefits></Benefits>
+      <Features></Features>
     </>
   )
 }
