67dc03cf89c6e69491b2c378250b335f3b82f5c8
Kalana <kalana33471@gmail.com>
2025-12-17 02:11:18 +0530

    bug fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,12 +27,12 @@ const Footer = () => {
                   <div>
                     <p className="text-primary text-sm mb-3 font-bold">Follow Us</p>
                     <div className="flex flex-col gap-2">
-                      <a href="#" className="flex items-center gap-2 text-gray-300 hover:text-white transition-colors text-sm">
+                      {/* <a href="#" className="flex items-center gap-2 text-gray-300 hover:text-white transition-colors text-sm">
                         <Facebook size={16} />
                         <span>Facebook</span>
-                      </a>
+                      </a> */}
                       
-                      <a href="#" className="flex items-center gap-2 text-gray-300 hover:text-white transition-colors text-sm">
+                      <a href="https://www.instagram.com/livecoach.ai?igsh=MTZpcWU1M2NyeXdseA==" className="flex items-center gap-2 text-gray-300 hover:text-white transition-colors text-sm">
                         <Instagram size={16} />
                         <span>Instagram</span>
                       </a>
